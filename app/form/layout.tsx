0b016b02e3c2b8eee5c4d536964c1643a83e9bcf
= <=>
2023-06-08 19:37:30 +0500

    C

--- app/form/layout.tsx
+++ app/form/layout.tsx
@@ -1,7 +1,5 @@
 import '../globals.css'
-import { Inter } from 'next/font/google'
 import Sidebar from "../components/Sidebar/Sidebar"
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
     title: 'Create Next App',
