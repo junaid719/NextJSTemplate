c00bc41c48b97d5da181c0b67d00653f4445d6d2
junaid719 <60477000+junaid719@users.noreply.github.com>
2023-06-15 14:48:52 +0500

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,8 @@ const authOptions: AuthOptions = {
     // Configure one or more authentication providers
     providers: [
         GithubProvider({
-            clientId: String(process.env.GITHUB_ID),
-            clientSecret: String(process.env.GITHUB_SECRET),
+            clientId: '2e1f530fe29e8f840aea',
+            clientSecret: '1bb9d51c9fd6a81f9babf0bcca2b2093fd048f67',
         }),
     ],
 
